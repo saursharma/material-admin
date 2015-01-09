6978eeb9ff562c257c24f9255450ad513e51e186
Saurabh Sharma <saursharma@google.com>
2015-01-09 19:34:47 +0530

    Including material icon repo (too big) and adding dialogs and floating icons

--- js/appController.js
+++ js/appController.js
@@ -1,13 +1,22 @@
-var adminPanel = angular.module('adminPanel',['ngMaterial']);
+var adminPanel = angular.module('adminPanel', ['ngMaterial'])
 
-adminPanel.controller('adminCtrl', function($scope, $mdSidenav) {
+adminPanel.controller('adminCtrl', function($scope, $mdSidenav, $mdDialog) {
   $scope.title = 'Admin Panel';
-
 
   $scope.toggleLeft = function() {
     $mdSidenav('left').toggle()
         .then(function(){
       $log.debug("toggle left is done");
+    });
+  };
+
+  // Show dialog
+  $scope.showDialog = function(ev) {
+    window.console.log('in func');
+    $mdDialog.show({
+     // controller: DialogController,
+      templateUrl: 'dialog.tmpl.html',
+      targetEvent: ev
     });
   };
 });
@@ -23,12 +32,45 @@ adminPanel.controller('LeftCtrl', function($scope, $timeout, $mdSidenav, $log) {
   // Icons at http://getbootstrap.com/components/
   $scope.menuItems = [{
     name : 'Home',
-    icon: 'glyphicon-home'
+    icon: 'glyphicon-home',
+    url: '#'
   }, {
     name : 'Settings',
-    icon: 'glyphicon-wrench'
+    icon: 'glyphicon-wrench',
+    url: '#apps'
   }, {
     name : 'LogOut',
-    icon: 'glyphicon-off'
+    icon: 'glyphicon-off',
+    url: '#'
   }];
-})
+});
+
+/**
+adminPanel.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+  .when('/', {
+      templateUrl : 'home.html',
+      controller: 'adminCtrl'
+  })
+  .when('/apps', {
+      templateUrl : 'apps.html',
+      controller: 'adminCtrl'
+  })
+  .otherwise({
+    redirectTo: '/'                 
+  });
+}]);
+*/
+
+// Timer and values for dashboard grid.
+setTimeout(function(){
+  document.getElementById('box1').innerHTML = 25;
+}, 1000);
+
+setTimeout(function(){
+  document.getElementById('box2').innerHTML = 456;
+}, 1000);
+
+setTimeout(function(){
+  document.getElementById('box3').innerHTML = 56;
+}, 1000);
