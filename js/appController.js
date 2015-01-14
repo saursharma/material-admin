389e859907b2d43fd0bd97405b53c0e76baca03a
Saurabh <saurabh@imaniac.org>
2015-01-14 23:56:07 +0530

    Adding routing

--- js/appController.js
+++ js/appController.js
@@ -1,6 +1,22 @@
-var adminPanel = angular.module('adminPanel',['ngMaterial']);
+var adminPanel = angular.module('adminPanel',['ngMaterial','ngRoute'])
 
-adminPanel.controller('loginCtrl', function($scope, $mdSidenav) {
+adminPanel.config(['$routeProvider' ,
+ function($routeProvider) {
+   $routeProvider
+  .when('/', {
+    templateUrl: 'index.html',
+    controller: 'adminCtrl'
+  })
+  .when('/login', {
+    templateUrl: 'login.html',
+    controller: 'adminCtrl'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+}]);
+
+adminPanel.controller('loginCtrl', function($scope, $mdSidenav) {i
   $scope.logo = "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xfa1/v/t1.0-1/p160x160/1966841_10205564593336771_1131066954403916535_n.jpg?oh=e110c4b435cec1f8d2d99b8fad29f516&oe=55336522&__gda__=1428491118_1bfc83d6ec955ffa3107d05ea7629090";
 });
 
@@ -14,6 +30,7 @@ adminPanel.controller('adminCtrl', function($scope, $mdSidenav) {
       $log.debug("toggle left is done");
     });
   };
+
 });
 
 adminPanel.controller('LeftCtrl', function($scope, $timeout, $mdSidenav, $log) {
